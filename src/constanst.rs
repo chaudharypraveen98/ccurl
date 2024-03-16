21db9ef6b206ce8646026c87ff5e8d1863e30d49
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-03-17 00:05:50 +0530

    put and get done

---
+++ src/constanst.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+
+
+lazy_static! {
+    pub static ref PROTOCOL_STRING: HashMap<&'static str, &'static str> = {
+        let mut map = HashMap::new();
+        map.insert("http", "HTTP/1.1");
+        map
+    };
+}
+pub const DEFAULT_PORT: &str = "80";
+pub static METHOD_TYPES: &[&str] = &[
+    "GET",
+    "POST",
+    "DELETE",
+    "PATCH",
+    "PUT",
+];
